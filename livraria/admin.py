26c42a2508dc80e3965b90dacfd28d823b22c840
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-03-17 14:08:27 -0300

    Criação da models Livro

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Categoria, Editora, Autor
+from .models import Categoria, Editora, Autor, Livro
 
 
 admin.site.register(Editora)
 admin.site.register(Categoria)
 admin.site.register(Autor)
+admin.site.register(Livro)
