2dac40016fcc082c70dc9cebd981ef314aaad520
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-04-04 15:01:11 -0300

    criação do ApiRest

--- livraria/models.py
+++ livraria/models.py
@@ -32,5 +32,5 @@ class Livro(models.Model):
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
 
     def __str__(self):
-        return f"{self.titulo}({self.quantidade})"
+        return f"{self.nome}({self.quantidade})"
 
