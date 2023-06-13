4a2a577a0068a11b78501609ea995f57f9c40d9e
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-06-13 14:40:09 -0300

    autores no livros

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Categoria, Editora
+from livraria.models import Categoria, Editora, Autor, Livro
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
@@ -11,3 +11,20 @@ class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
         fields = "__all__"
+
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1        
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1        
--- livraria/views.py
+++ livraria/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria, Editora
-from livraria.serializers import CategoriaSerializer , EditoraSerializer
+from livraria.models import Categoria, Editora, Autor , Livro
+from livraria.serializers import CategoriaSerializer , EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
@@ -11,6 +11,18 @@ class CategoriaViewSet(ModelViewSet):
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer  
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return LivroDetailSerializer
+        return LivroSerializer    
 
     
 # Create your views here.
