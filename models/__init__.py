1188b884ff6c397273ff89b6a364e3cb2e9a0089
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-06-27 14:19:17 -0300

    Separação de pastas via models

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .autor import Autor
+from .livro import Livro
+from .categoria import Categoria
+from .editora import Editora
---
+++ models/editora.py
@@ -0,0 +1,7 @@
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)#esse campo e opcional
+
+    def __str__(self):
+        return self.nome
+    
